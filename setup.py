b6c72fc823a1ad0ec5602bce4033ad7cff8865b2
aparnasharma4488-ui <aparnasharma.4488@gmail.com>
2026-03-11 18:06:04 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import  find_packages, setup
+
+
+setup(
+    name='generative_AI_Project',
+    version='0.0.0',
+    author='Aparna Sharma',
+    author_email='aparnasharma.4488@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
